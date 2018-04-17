f4d7d1df3eae7c8a67a65ef8035315907946d3df
Davi T. Ferreira <ferreira.tavares.davi@gmail.com>
2018-04-17 14:05:35 -0300

    a mais recente atualização do service worker

--- sw.js
+++ sw.js
@@ -14,53 +14,51 @@ self.addEventListener('install', function (event) {
                 '/magnolia/matricula_google.html',
                 '/magnolia/sobre.html',
                 '/magnolia/contato.html',
-                'magnolia/img/mag-logo.png',
-                'magnolia/img/cinema.jpg',
-                'magnolia/img/educacao.jpeg',
-                'magnolia/img/humilde.png',
-                'magnolia/img/icon.png',
-                'magnolia/img/literatura.jgg',
-                'magnolia/img/livia.jpg',
-                'magnolia/img/music.jpg',
-                'magnolia/img/pagano.jpg',
-                'magnolia/img/pintura.jpg',
-                'magnolia/img/producao.jpg',
-                'magnolia/img/sad.png',
-                'magnolia/img/teatro.jpg',
-                'magnolia/img/thalis.jpg',
-                'magnolia/css/style.css',
-                'magnolia/sw.js',
-                'magnolia/service.js'
+                '/magnolia/img/mag-logo.png',
+                '/magnolia/img/cinema.jpg',
+                '/magnolia/img/educacao.jpeg',
+                '/magnolia/img/humilde.png',
+                '/magnolia/img/icon.png',
+                '/magnolia/img/literatura.jgg',
+                '/magnolia/img/livia.jpg',
+                '/magnolia/img/music.jpg',
+                '/magnolia/img/pagano.jpg',
+                '/magnolia/img/pintura.jpg',
+                '/magnolia/img/producao.jpg',
+                '/magnolia/img/sad.png',
+                '/magnolia/img/teatro.jpg',
+                '/magnolia/img/thalis.jpg',
+                '/magnolia/css/style.css',
+                '/magnolia/sw.js',
+                '/magnolia/service.js'
             ])
         })
     )
 });
 
-self.addEventListener('install', function (event) {
-    var offlinePage = new Request('offline.html');
+self.addEventListener('activate', function activator(event) {
     event.waitUntil(
-        fetch(offlinePage).then(function (response) {
-            return caches.open('pwabuilder-offline').then(function (cache) {
-                console.log('[PWA Builder] Cached offline page during Install' + response.url);
-                return cache.put(offlinePage, response);
-            });
-        }));
+        caches.keys().then(function (keys) {
+            return Promise.all(keys
+                .filter(function (key) {
+                    return key.indexOf(CACHE_NAME) !== 0;
+                })
+                .map(function (key) {
+                    return caches.delete(key);
+                })
+            );
+        })
+    );
 });
-
 
 self.addEventListener('fetch', function (event) {
     event.respondWith(
-        fetch(event.request).catch(function (error) {
-            console.error('[PWA Builder] Network request Failed. Serving offline page ' + error);
-            return caches.open('pwabuilder-offline').then(function (cache) {
-                return cache.match('offline.html');
-            });
-        }));
+        caches.match(event.request).then(function (cachedResponse) {
+            return cachedResponse || fetch(event.request);
+        })
+    );
 });
 
-self.addEventListener('refreshOffline', function (response) {
-    return caches.open('pwabuilder-offline').then(function (cache) {
-        console.log('[PWA Builder] Offline page updated from refreshOffline event: ' + response.url);
-        return cache.put(offlinePage, response);
-    });
-});
+
+
+
